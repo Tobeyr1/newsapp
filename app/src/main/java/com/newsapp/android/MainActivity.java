2220ea2c36a11af1f642b50bf01c123fc11f7c9d
liuzirui <rui@gmail.com>
2019-04-05 07:31:26 +0800

    Newsapp

--- app/src/main/java/com/newsapp/android/MainActivity.java
+++ app/src/main/java/com/newsapp/android/MainActivity.java
@@ -1,11 +1,14 @@
 package com.newsapp.android;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
+import com.newsapp.android.exitsettings.ActivityCollector;
 import com.newsapp.android.exitsettings.BasicActivity;
 
 import org.litepal.LitePal;
@@ -26,5 +29,24 @@ public class MainActivity extends BasicActivity {
                 Toast.makeText(MainActivity.this,"good job",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.userinfom:
+                Toast.makeText(this,"ni click 用户设置",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.usersettings:
+                Toast.makeText(this,"退出",Toast.LENGTH_SHORT).show();
+                ActivityCollector.finishAll();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
