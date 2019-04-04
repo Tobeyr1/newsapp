f137027cf38653e475234b677c105d11cc897e36
liuzirui <rui@gmail.com>
2019-04-04 14:50:42 +0800

    标题美化

--- app/src/main/java/com/newsapp/android/MainActivity.java
+++ app/src/main/java/com/newsapp/android/MainActivity.java
@@ -2,12 +2,28 @@ package com.newsapp.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        button = (Button)findViewById(R.id.button_pal);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            /*LitePal.getDatabase();*/
+                Toast.makeText(MainActivity.this,"good job",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/newsapp/android/db/Userinfo.java
@@ -0,0 +1,20 @@
+package com.newsapp.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Userinfo extends LitePalSupport {
+    private String name;
+    private String password;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public  String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+}
