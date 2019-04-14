c654ccb5d8103335385a3b9690b938325006208b
liuzirui <rui@gmail.com>
2019-04-14 09:13:28 +0800

    Newsapp

--- app/src/main/java/com/newsapp/android/MainActivity.java
+++ app/src/main/java/com/newsapp/android/MainActivity.java
@@ -1,6 +1,13 @@
 package com.newsapp.android;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -8,19 +15,49 @@ import android.widget.Button;
 import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
+import com.newsapp.android.ViewPageTitle.TabAdapter;
 import com.newsapp.android.exitsettings.ActivityCollector;
 import com.newsapp.android.exitsettings.BasicActivity;
+import com.viewpagerindicator.TabPageIndicator;
 
 import org.litepal.LitePal;
 
 public class MainActivity extends BasicActivity {
+    private TabPageIndicator mIndicator;
+    private ViewPager mViewPager;
+    private FragmentPagerAdapter mAdapter;
+    private DrawerLayout mDrawerLayout;
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mIndicator = (TabPageIndicator) findViewById(R.id.id_indicator);
+        mViewPager = (ViewPager) findViewById(R.id.id_pager);
+        mAdapter = new TabAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mAdapter);
+        mIndicator.setViewPager(mViewPager, 0);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
+        }
+        navView.setCheckedItem(R.id.nav_call);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+               mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
         button = (Button)findViewById(R.id.button_pal);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +83,11 @@ public class MainActivity extends BasicActivity {
             case R.id.usersettings:
                 Toast.makeText(this,"退出",Toast.LENGTH_SHORT).show();
                 ActivityCollector.finishAll();
+                break;
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+                default:
         }
         return super.onOptionsItemSelected(item);
     }
